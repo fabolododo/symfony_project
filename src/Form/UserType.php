a36695edf25dfd86e5739f128a74bf2ccd9a8b5e
fabolododo <fabolododo@hotmail.fr>
2019-08-07 18:28:27 +0200

    modified UserType.php

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,15 +7,58 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('password')
             ->add('username')
-            ->add('updated_at')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new Email(['message' => 'Please enter a valid email address.'])
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'required' => false,
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'constraints' => [
+                    // new NotBlank([
+                        // 'message' => 'Please enter a password',
+                    // ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+                'invalid_message' => 'Your password does not match the confirmation.'
+            ])
+            ->add('admin')
+
+            ->add('Confirmation_Box', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You must click to confirm.',
+                    ]),
+                ],
+            ])
         ;
     }
 

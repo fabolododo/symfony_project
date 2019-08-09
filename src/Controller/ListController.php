4e0b3850e470b9d3ddf31709214f909e39c3722e
fabolododo <fabolododo@hotmail.fr>
2019-08-09 18:25:30 +0200

    modified ListController.php

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpClient\HttpClient;
+
 class ListController extends AbstractController
 {
     /**
@@ -12,22 +13,27 @@ class ListController extends AbstractController
      */
     public function list()
     {
-        $httpClient = HttpClient::create();
-        $response = $httpClient->request('GET', 'http://www.omdbapi.com/?apikey=5a61096e&t=Star-Wars:-Episode-V');
-        // var_dump($response->toArray());
-        $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
+        $title_choice = ['death-proof','fight-club','v-for-vendetta','star-wars:-episode-IV','star-wars:-episode-V','Avengers:-EndGame'];
+        $resultat = [];
+        for ($i = 0; $i < count($title_choice); $i++){
+            $httpClient = HttpClient::create();
+            $response = $httpClient->request('GET', 'http://www.omdbapi.com/?apikey=5a61096e&t='.$title_choice[$i]);
+            // var_dump($response->toArray());
+            $statusCode = $response->getStatusCode();
+            // $statusCode = 200
+            $contentType = $response->getHeaders()['content-type'][0];
+            // $contentType = 'application/json'
+            $content = $response->getContent();
+            // $content = '{"id":521583, "name":"symfony-docs", ...}'
+            $content = $response->toArray();
+            // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
 
+            array_push($resultat,$content);
+        }
 
-        return $this->render('list/index.html.twig', [
+        return $this->render('list/list.html.twig', [
             'controller_name' => 'ListController',
-            'content' => $content
+            'resultat' => $resultat
         ]);
     }
 }

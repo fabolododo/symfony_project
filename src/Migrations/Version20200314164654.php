2c5098f522d69833708319eae5dbb4358a60c615
FabriceL <fabrice.latri@hotmail.fr>
2020-03-14 18:06:27 +0100

    change Migrations Version

--- src/Migrations/Version20200314164654.php
+++ src/Migrations/Version20200314164654.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190809124726 extends AbstractMigration
+final class Version20200314164654 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,8 +22,9 @@ final class Version20190809124726 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE movie_list (id INT AUTO_INCREMENT NOT NULL, title_id INT DEFAULT NULL, INDEX IDX_B7AED915A9F87BD (title_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, admin TINYINT(1) DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE movies (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, content VARCHAR(255) NOT NULL, release_date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE movie_list (id INT AUTO_INCREMENT NOT NULL, title_id INT DEFAULT NULL, INDEX IDX_B7AED915A9F87BD (title_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE movie_list ADD CONSTRAINT FK_B7AED915A9F87BD FOREIGN KEY (title_id) REFERENCES user (id)');
     }
 
@@ -32,7 +33,9 @@ final class Version20190809124726 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE movie_list');
+        $this->addSql('ALTER TABLE movie_list DROP FOREIGN KEY FK_B7AED915A9F87BD');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE movies');
+        $this->addSql('DROP TABLE movie_list');
     }
 }

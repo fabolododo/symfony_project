692623b5d0dc23adea7ab7ba73c8d4dce8fe7317
Fabrice Latri <fabrice.latri@hotmail.fr>
2020-08-27 19:36:53 +0200

    Comments

--- src/Repository/MovieListRepository.php
+++ src/Repository/MovieListRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\MovieList;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method MovieList|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MovieListRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MovieList::class);
     }
--- src/Repository/MoviesRepository.php
+++ src/Repository/MoviesRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Movies;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Movies|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class MoviesRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Movies::class);
     }

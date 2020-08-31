f05d72d72d09e0d561992a2b61b6e57bd34924e9
Fabrice Latri <fabrice.latri@hotmail.fr>
2020-08-31 21:25:02 +0200

    Fix bug ManagerRegistry

--- src/Repository/MovieListRepository.php
+++ src/Repository/MovieListRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\MovieList;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method MovieList|null find($id, $lockMode = null, $lockVersion = null)
